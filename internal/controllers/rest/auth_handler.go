4f16cd4525baed414a33a7b639c0bcd811d7bb32
VanLavr <lavr.i-n@mail.ru>
2025-05-03 22:58:04 +0300

    feat: auth seems to be done

--- internal/controllers/rest/auth_handler.go
+++ internal/controllers/rest/auth_handler.go
@@ -1,7 +1,6 @@
 package rest
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -28,8 +27,7 @@ func NewAuthHandler(teacherUsecase logic.TeacherUsecase, studentUsecase logic.St
 }
 
 func (a AuthHandler) RegisterRoutes(group *gin.RouterGroup) {
-	group.POST("/teacher/login", a.TeacherLogin)
-	group.POST("/student/login", a.StudentLogin)
+	group.POST("/login", a.Login)
 
 	permissions := group.Group("/perm/check", a.auth.ValidateAccessToken())
 	permissions.GET("/current_permissions", a.GetCurrentPermissions)
@@ -39,7 +37,7 @@ func (a AuthHandler) GetCurrentPermissions(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{auth.RoleKey: c.Value(auth.RoleKey)})
 }
 
-func (a AuthHandler) TeacherLogin(c *gin.Context) {
+func (a AuthHandler) Login(c *gin.Context) {
 	var request dto.LoginDTO
 	if err := c.Bind(&request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -55,44 +53,6 @@ func (a AuthHandler) TeacherLogin(c *gin.Context) {
 		})
 		return
 	}
-
-	if len(teachers) == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
-		return
-	}
-
-	if !hasher.Hshr.Validate(request.Password, teachers[0].Password) {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": errors.ErroInvalidPassword,
-		})
-		return
-	}
-
-	token, err := a.auth.GenerateAccessToken(teachers[0].UUID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err,
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"access_token": token,
-	})
-}
-
-func (a AuthHandler) StudentLogin(c *gin.Context) {
-	var request dto.LoginDTO
-	if err := c.Bind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
-		return
-	}
-	fmt.Println(request)
-
 	students, err := a.studentUsecase.GetStudentByEmail(c.Request.Context(), request.Email)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -100,31 +60,51 @@ func (a AuthHandler) StudentLogin(c *gin.Context) {
 		})
 		return
 	}
-	fmt.Println("got")
+
+	if len(teachers) == 0 && len(students) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": errors.ErroNoItemsFound,
+		})
+		return
+	}
 
 	if len(students) == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
-		return
-	}
+		if !hasher.Hshr.Validate(request.Password, teachers[0].Password) {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": errors.ErroInvalidPassword,
+			})
+			return
+		}
 
-	if !hasher.Hshr.Validate(request.Password, students[0].Password) {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": errors.ErroInvalidPassword,
-		})
-		return
-	}
+		token, err := a.auth.GenerateAccessToken(teachers[0].UUID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
 
-	token, err := a.auth.GenerateAccessToken(students[0].UUID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err,
+		c.JSON(http.StatusOK, gin.H{
+			"access_token": token,
 		})
-		return
-	}
+	} else {
+		if !hasher.Hshr.Validate(request.Password, students[0].Password) {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": errors.ErroInvalidPassword,
+			})
+			return
+		}
 
-	c.JSON(http.StatusOK, gin.H{
-		"access_token": token,
-	})
+		token, err := a.auth.GenerateAccessToken(students[0].UUID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"access_token": token,
+		})
+	}
 }
